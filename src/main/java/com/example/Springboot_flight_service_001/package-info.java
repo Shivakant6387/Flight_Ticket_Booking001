dcfc98db001ad27f79c09a29f38c2e40b2dd294a
Shivakant6387 <shivakant5875@gmail.com>
2023-03-30 10:52:26 +0530

    pom xml dependences changes

--- src/main/java/com/example/Springboot_flight_service_001/package-info.java
+++ src/main/java/com/example/Springboot_flight_service_001/package-info.java
@@ -15,3 +15,20 @@ package com.example.Springboot_flight_service_001;
 //        "cardType":"DEBIT"
 //        }
 //        }
+
+//{
+//        "passengerInfo":{
+//        "name":"basant",
+//        "email":"basant@gmail.com",
+//        "source":"Bangalore",
+//        "Destination":"RBSC",
+//        "travelDate":"10-12-2022",
+//        "pickupTine":"4.0 PM",
+//        "arrivallise":"6.0 PM",
+//        " fare":10000.0
+//        },
+//        "paymentInfo":{
+//        "accountNo":"acc2",
+//        "cardType":"DEBIT"
+//        }
+//        }
